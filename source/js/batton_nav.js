7c974379b0d3f2e2d5c20dced63246119da40fd0
Андрей <cereperec@gmail.com>
2020-09-01 23:08:22 +0300

    final

--- source/js/batton_nav.js
+++ source/js/batton_nav.js
@@ -5,6 +5,8 @@ var navButton = document.querySelector('.main-nav__button');
 var navButton2 = document.querySelector('.main-nav__button--btn');
 
 navButton2.classList.remove('main-nav__button--btn');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
 
 navButton.addEventListener('click', function() {
   navMain.classList.toggle ('main-nav--closed');
