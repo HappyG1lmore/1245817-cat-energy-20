2e91238dddb74046d74c800772e7c41db643bc2d
Андрей <cereperec@gmail.com>
2020-09-14 17:21:31 +0300

    final 3.1

--- source/js/cat.js
+++ source/js/cat.js
@@ -4,15 +4,24 @@ const afterCat = document.querySelector(".exemplar__picture--after");
 const beforeBtn = document.querySelector(".exemplar__btn-result--before");
 const afterBtn = document.querySelector(".exemplar__btn-result--after");
 
+const togle = document.querySelector(".exemplar__togle");
+const togleAfter = document.querySelector(".exemplar__togle--after");
+
 beforeBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
+  console.log('click');
   beforeCat.classList.remove("visuallyhidden");
   afterCat.classList.add("visuallyhidden");
+  togle.classList.remove("exemplar__togle--after");
 })
 
 afterBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
+  console.log('click');
   afterCat.classList.remove("visuallyhidden");
   beforeCat.classList.add("visuallyhidden");
+  togle.classList.add("exemplar__togle--after");
 })
+
+
 
